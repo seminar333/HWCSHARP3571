322640c8c46e93460941538c6322e3e8facea3d6
graf_orlov <graf_orlov88@mail.ru>
2022-12-04 20:09:26 +0300

    исправил ошибкуt19

--- hws3/hws3t19/Program.cs
+++ hws3/hws3t19/Program.cs
@@ -41,26 +41,46 @@ else
 
     int i = 1;
 
+    int[] array = new int[count];
+    int index = 0;
     int leftnumber = 0;
     int rightnumber = 0;
     int k = 1;
     int leftres = 1;
     int rightres = 1;
     int n = 1;
+    int templ = 1;// можно и без них
+    int tempr = 1;//можно и без них
+    // int templ1 = 1;
+    // int tempr1 = 1;
     while (i <= count / 2)
     {
 
         Int32 gapl = Convert.ToInt32(Math.Pow(10, count - i));
         leftnumber = data / gapl;
 
+
+
         if (i >= 2)
         {
+
+
+
             leftres = leftnumber % 10;
+            array[index] = leftres;
+
         }
         else
         {
             leftres = leftnumber;
+            templ = leftres;
+            array[0] = templ;
+
+
+
+
         }
+
         Console.WriteLine($"цифра {n} слева, {leftres}");
 
         Int32 gapr = Convert.ToInt32(Math.Pow(10, i));
@@ -71,29 +91,47 @@ else
 
             Int32 r = Convert.ToInt32(Math.Pow(10, k));
             rightres = rightnumber / r;
+            array[count - 1 - index] = rightres;
             k++;
+
+
+
         }
         else
         {
 
             rightres = rightnumber;
+            tempr = rightres;
+            array[count - 1] = tempr;
 
         }
+
+        
+    
         Console.WriteLine($"цифра {n} справа, {rightres}");
+        index++;
         i++;
         n++;
 
     }
 
-    if (leftres == rightres)
+    // if (leftres == rightres && tempr == templ)
+    // {
+    //     Console.WriteLine("число является палиндромом");
+
+    // }
+
+    // else
+    // {
+
+    //     Console.WriteLine("число НЕ является палиндромом");
+    // }
+
+    for (index = 0; index < count; index++)
     {
-        Console.WriteLine("число является палиндромом");
-
+        Console.WriteLine(array[index]);
     }
-
-    else
-    {
-
-        Console.WriteLine("число НЕ является палиндромом");
-    }
+    
+   if (array[0] == array[4] && array[1] == array[3]&&array[2] == array[2]) {Console.WriteLine ("палиндром");}
+   else {Console.WriteLine("не палиндром");}
 }
